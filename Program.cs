64ca8ccbd97f0c59d5e2e299c949149a2afc5ea3
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2023-11-21 13:32:25 +0200

    ♻️ Make it more user friendly

--- Program.cs
+++ Program.cs
@@ -1,47 +1,81 @@
-﻿namespace Calculator
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int num1;
-            int num2;
-            string? oper;
-            int result = 0;
-            // Message
-            Console.WriteLine("Calculator");
-            // Get Numbers
-            Console.WriteLine("Enter First Number: ");
-            num1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter Second Number: ");
-            num2 = Convert.ToInt32(Console.ReadLine());
-            // Get oper
-            Console.WriteLine("Enter a for addition, s for subtraction, m for multiplication, or d for division.");
-            oper = Console.ReadLine();
-            Console.WriteLine(oper);
+﻿using System;
 
-            if (oper == "a")
+class Calculator
+{
+    static void Main()
+    {
+        Console.WriteLine("Simple Calculator");
+
+        while (true)
+        {
+            Console.WriteLine("\n1. Addition");
+            Console.WriteLine("2. Subtraction");
+            Console.WriteLine("3. Multiplication");
+            Console.WriteLine("4. Division");
+            Console.WriteLine("5. Exit");
+
+            Console.Write("Enter your choice (1-5): ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
             {
-                result = num1 + num2;
+                case "1":
+                    PerformOperation(Addition);
+                    break;
+                case "2":
+                    PerformOperation(Subtraction);
+                    break;
+                case "3":
+                    PerformOperation(Multiplication);
+                    break;
+                case "4":
+                    PerformOperation(Division);
+                    break;
+                case "5":
+                    Console.WriteLine("Exiting the calculator. Goodbye!");
+                    return;
+                default:
+                    Console.WriteLine("Invalid choice. Please enter a number between 1 and 5.");
+                    break;
             }
-            else if (oper == "s")
+        }
+    }
+
+    static void PerformOperation(Func<double, double, double> operation)
+    {
+        Console.Write("Enter the first number: ");
+        if (double.TryParse(Console.ReadLine(), out double num1))
+        {
+            Console.Write("Enter the second number: ");
+            if (double.TryParse(Console.ReadLine(), out double num2))
             {
-                result = num1 - num2;
-            }
-            else if (oper == "m")
-            {
-                result = num1 * num2;
-            }
-            else if (oper == "d")
-            {
-                result = num1 / num2;
+                double result = operation(num1, num2);
+                Console.WriteLine($"Result: {result}");
             }
             else
             {
-                Console.WriteLine("Enter a for addition, s for subtraction, m for multiplication, or d for division.");
+                Console.WriteLine("Invalid input for the second number.");
             }
-            Console.WriteLine("The Result: " + result);
+        }
+        else
+        {
+            Console.WriteLine("Invalid input for the first number.");
+        }
+    }
 
+    static double Addition(double num1, double num2) => num1 + num2;
+    static double Subtraction(double num1, double num2) => num1 - num2;
+    static double Multiplication(double num1, double num2) => num1 * num2;
+    static double Division(double num1, double num2)
+    {
+        if (num2 == 0)
+        {
+            Console.WriteLine("Error: Division by zero is undefined.");
+            return 0;
+        }
+        else
+        {
+            return num1 / num2;
         }
     }
 }
